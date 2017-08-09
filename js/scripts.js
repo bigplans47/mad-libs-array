6b7f94557bf5535645ee32675fad3cb4fc752c9e
Daniel Kiss and Dawson Mortenson <dawson@>
2017-08-09 09:39:15 -0700

    create a mad libs game that demonstrates forms

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var person1input = $("input#person1").val();
+    var person2input = $("input#person2").val();
+    var animalInput = $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
+
+    $(".person1").text(person1input);
+    $(".person2").text(person2input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
